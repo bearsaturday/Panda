9a71bd29bddfc3a5a56acf96c209b4a5b92db924
Akihito Koriyama <akihito.koriyama@gmail.com>
2011-06-17 06:36:44 +0000

    replace bespin to ace

--- data/htdocs/__panda/edit/save.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$path = $_POST['path'];
-$data = $_POST['data'];
-$result = file_put_contents($path, $data, LOCK_EX | FILE_TEXT);
-$msg = ($result === false) ? 'save error..' : ' saved';
-echo $msg;
